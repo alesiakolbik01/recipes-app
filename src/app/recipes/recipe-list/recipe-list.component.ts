cef9bdf47f879778cd31df1b7424d202111c7765
Alesia Kolbik <alesyakolbik@gmail.com>
2024-01-22 19:49:51 +0100

    added templates, models of components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './recipe-list.component.scss'
 })
 export class RecipeListComponent {
-
+  recipes:Recipe[] = [{
+    id: 1,
+    name: "Test recipe",
+    description: "this recipe description",
+    imagePath: "https://www.cookingclassy.com/wp-content/uploads/2019/09/meatballs-21.jpg",
+    createDate: new Date()
+  }];
 }
